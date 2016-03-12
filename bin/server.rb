145a723da9183058ce5194b7da076b5b00bdeb99
Brian Lambert <Brian.joseth.lambert@gmail.com>
2016-03-12 14:22:45 -0800

    clean up and merge servers

--- bin/server.rb
+++ bin/server.rb
@@ -1,6 +1,20 @@
 require 'rack'
+require 'erb'
 require_relative '../lib/router'
+require_relative '../lib/controller_base'
 require_relative '../lib/show_exceptions'
+require_relative '../lib/static'
+
+class MyController < ControllerBase
+  def stage
+    render_content("Gambian takes the stage", "text/html")
+  end
+end
+
+router = Router.new
+router.draw do
+  get Regexp.new("^/$"), MyController, :stage
+end
 
 app = Proc.new do |env|
   req = Rack::Request.new(env)
@@ -11,6 +25,7 @@ end
 
 app = Rack::Builder.new do
   use ShowExceptions
+  use Static
   run app
 end.to_app
 
